a3569b07f55ff7fc049d742c669390727aca0f73
kmss6905 <kmss69052@naver.com>
2023-11-29 23:56:00 +0900

    Feature: Budget 에 BudgetCategory 추가 하는 서비스

--- src/main/java/jaringobi/dto/request/BudgetByCategoryRequest.java
+++ src/main/java/jaringobi/dto/request/BudgetByCategoryRequest.java
@@ -2,6 +2,8 @@ package jaringobi.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jaringobi.domain.budget.CategoryBudget;
+import jaringobi.domain.budget.Money;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,4 +19,11 @@ public class BudgetByCategoryRequest {
 
     @Min(value = 1, message = "{value} 이상 이어야 합니다.")
     private int money;
+
+    public CategoryBudget toCategoryBudget() {
+        return CategoryBudget.builder()
+                .categoryId(categoryId)
+                .amount(new Money(money))
+                .build();
+    }
 }
